5e148147f5c1c705cab680ddd62027b5092da6a1
Abir Shahriar <67016051+TheMAbir@users.noreply.github.com>
2024-05-05 12:53:05 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,15 @@ import os
 # Install dependencies
 os.system("pip install git+https://github.com/openai/CLIP.git")
 os.system("pip install sentence-transformers")
-
+os.system("pip install torch")
+os.system("pip install pillow")
+os.system("pip install request")
+os.system("pip install numpy")
+os.system("pip install fuzzywuzzy")
+os.system("pip install python-Levenshtein")
+os.system("pip install sentence-transformers")
+os.system("pip install sentence-transformers")
+os.system("pip install sentence-transformers")
 setuptools.setup(
     name="FuzzyZSIC",
     version="1.0",
@@ -12,16 +20,7 @@ setuptools.setup(
     description="Fuzzy Zeroshot Image Classification",
     url="https://github.com/TheMAbir/FuzzyZSIC",
     packages=setuptools.find_packages(),
-    install_requires=[
-        "torch",
-        "clip",
-        "Pillow",
-        "requests",
-        "numpy",
-        "sentence-transformers",
-        "fuzzywuzzy",
-        "python-Levenshtein"
-    ],
+    install_requires=[],
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
